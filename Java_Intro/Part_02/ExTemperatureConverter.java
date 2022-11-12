72b62ec646ba1f66a4cc2d4d2a4d3891c6aa97e0
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-11-12 08:22:29 -0300

    Delete Java_Intro/Part_02 directory

--- Java_Intro/Part_02/ExTemperatureConverter.java
+++
@@ -1,9 +0,0 @@
-class ExTemperatureConverter {
-  public static void main(String[] args) {
-    
-    double tf = 84.5;
-    
-    double tc = (tf - 32) / 9 * 5;
-    System.out.println("A temperatura em Celsius é: " + tc);
-  }
-}
