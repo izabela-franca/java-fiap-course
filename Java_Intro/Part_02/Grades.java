c10cab055c3aa7cc69c2364b441a7ec4bbafcf91
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-11-12 08:24:03 -0300

    Update Grades.java

--- Java_Intro/Part_02/Grades.java
+++ Java_Intro/Part_02/Grades.java
@@ -1,12 +1,12 @@
 class Grades {
   public static void main(String[] args) {
     
-    int n1 = 4;
-    int n2 = 7;
-    int n3 = 8;
-    int n4 = 6;
+    double n1 = 4;
+    double n2 = 7;
+    double n3 = 8;
+    double n4 = 6;
     
-    double media = (double) (n1 + n2 + n3 + n4) / 4;
+    double media = (n1 + n2 + n3 + n4) / 4;
     System.out.println("A média é " + media);
   }
 }
