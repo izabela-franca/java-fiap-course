9accfc8e2819912ea897352e8aeb8d39822176d6
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-11-25 10:49:24 -0300

    Add files via upload

---
+++ java-essentials/java-core/Variables/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String s1 = "abc";
+        String s2 = "FFF";
+        String s3 = s1 + s2;
+        s3 = s3.toUpperCase();
+
+        System.out.println(s3);
+    }
+}
