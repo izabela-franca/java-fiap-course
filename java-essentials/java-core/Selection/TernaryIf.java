674287ad5a034afe6ec7c651761daee91c98c3a1
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-11-27 10:15:41 -0300

    Add files via upload

---
+++ java-essentials/java-core/Selection/TernaryIf.java
@@ -0,0 +1,11 @@
+public class TernaryIf {
+
+    public static void main(String[] args) {
+
+        int age = 10;
+
+                       //Condition      //true          // false
+        String text = age >= 18 ? "Maior de idade" : "Menor de idade";
+        System.out.println(text);
+    }
+}
