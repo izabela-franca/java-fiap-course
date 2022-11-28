649cae14c77c81a96eb687780b18050bc9731379
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-11-28 11:02:45 -0300

    Add files via upload

---
+++ java-essentials/java-core/Repetition/InnerLoops.java
@@ -0,0 +1,19 @@
+public class InnerLoops {
+
+    public static void main(String[] args) {
+
+        //Naming the loop
+        outer:
+        for (int i = 1; i <= 10; i++) {
+
+            inner:
+            for (int j = 1; j <= 10; j++) {
+                if (j == 5) {
+                    continue outer;
+                }
+                System.out.println(i + j);
+            }
+        }
+        System.out.println("FIM");
+    }
+}
---
+++ java-essentials/java-core/Repetition/RepeatWhileDoWhile.java
@@ -0,0 +1,22 @@
+public class RepeatWhileDoWhile {
+
+    public static void main(String[] args) {
+
+        System.out.println("WHILE");
+        int i = 0;
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("FIM");
+
+        System.out.println("DO..WHILE");
+        int j = 0;
+
+        do {
+            System.out.println(j);
+            j++;
+        } while (j <= 10);
+    }
+}
