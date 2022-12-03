4439179ca633799c635649bfb3a5c96403047e60
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-12-03 14:38:48 -0300

    Update Account.java

--- java-essentials/java-oo-1/Account/Account.java
+++ java-essentials/java-oo-1/Account/Account.java
@@ -6,14 +6,34 @@ public class Account {
     double balance;
 
     //Creating methods
-    void deposit(double amount) {
+    boolean deposit(double amount) {
         if (amount > 0) {
             balance += amount;
+            System.out.println("Depósito realizado com sucesso!");
+            return true;
         }
+        System.out.println("Ops, depósito inválido!");
+        return false;
     }
 
-    void withdraw(double amount) {
-        balance -= amount;
+    //Method Overloading
+    boolean deposit(String amount) {
+        return  deposit(Double.parseDouble(amount));
+    }
+
+    boolean withdraw(double amount) {
+        if (amount > 0) {
+            balance -= amount;
+            System.out.println("Saque realizado com sucesso!");
+            return  true;
+        }
+        System.out.println("Ops, saque inválido!");
+        return  false;
+    }
+
+    void transfer(double amount, Account targetAccount) {
+        withdraw(amount);
+        targetAccount.deposit(amount);
     }
 
     void printBalance() {
