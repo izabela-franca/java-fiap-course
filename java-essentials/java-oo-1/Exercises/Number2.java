8d74c22175224726d88d4bbd6ea7d4ae58906fd4
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-12-05 09:44:59 -0300

    Add files via upload

---
+++ java-essentials/java-oo-1/Exercises/Number2.java
@@ -0,0 +1,25 @@
+public class Number2 {
+    private static int instances;
+    private final int number;
+
+    private Number2(int number) {
+        this.number = number;
+        instances++;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public static Number2 add(Number2 n1, Number2 n2) {
+        return new Number2(n1.getNumber() + n2.getNumber());
+    }
+
+    public static int getInstances() {
+        return instances;
+    }
+
+    public static Number2 newNumber(int n) {
+        return new Number2(n);
+    }
+}
