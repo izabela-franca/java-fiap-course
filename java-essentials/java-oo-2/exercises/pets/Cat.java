bf161ecfac604b442323bf41562408e58659329a
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-12-10 10:15:16 -0300

    Update Cat.java

--- java-essentials/java-oo-2/exercises/pets/Cat.java
+++ java-essentials/java-oo-2/exercises/pets/Cat.java
@@ -6,4 +6,10 @@ public class Cat extends TerrestrialAnimal{
     public void talk() {
         System.out.println("Meow meow!");
     }
+
+    //An implementation is necessary due to the fact that the super class is abstract
+    @Override
+    public void sleep(int time) {
+        System.out.println("Cat's sleeping.");
+    }
 }
