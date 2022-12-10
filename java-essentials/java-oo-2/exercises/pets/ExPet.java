eb9b6f77d9b29c4f7478a93fdf36ec3d057d9d97
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-12-10 10:16:05 -0300

    Update ExPet.java

--- java-essentials/java-oo-2/exercises/pets/ExPet.java
+++ java-essentials/java-oo-2/exercises/pets/ExPet.java
@@ -9,16 +9,23 @@ public class ExPet {
         pet1.setColor("black");
         pet1.talk();
         pet1.walk();
+        pet1.feed(10);
+        pet1.feed(5);
+        System.out.println(pet1.getAmountEaten());
 
         System.out.println("---> Cat");
         Cat pet2 = new Cat();
         pet2.setColor("White");
         pet2.talk();
         pet2.walk();
+        pet2.feed(5);
+        pet2.feed(9);
+        System.out.println(pet2.getAmountEaten());
 
         System.out.println("---> Fish");
         Fish pet3 = new Fish();
         pet3.setColor("Orange");
         pet3.swim();
+
     }
 }
