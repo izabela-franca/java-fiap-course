89b70ebaaac9a33016d30b4ff4ad31a1b12b92a3
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-12-10 10:16:38 -0300

    Update Pet.java

--- java-essentials/java-oo-2/exercises/pets/Pet.java
+++ java-essentials/java-oo-2/exercises/pets/Pet.java
@@ -1,8 +1,9 @@
 package exercises.pets;
 
-public class Pet {
+public abstract class Pet { //Abstract class: it cannot be instantiated
 
     private String color;
+    private int amountEaten;
 
     public String getColor() {
         return color;
@@ -11,4 +12,15 @@ public class Pet {
     public void setColor(String color) {
         this.color = color;
     }
+
+    public void feed(int amount) {
+        amountEaten += amount;
+    }
+
+    public int getAmountEaten() {
+        return amountEaten;
+    }
+
+    //An abstract method doesn't need implementation
+    public abstract void sleep(int time);
 }
