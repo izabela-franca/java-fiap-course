840535e4757cbd0c47d9a8757c426622701279b8
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-04-11 10:06:47 -0300

    Update AppCalc2.java

--- java-professional-1/java-lambda/AppCalc2.java
+++ java-professional-1/java-lambda/AppCalc2.java
@@ -2,6 +2,8 @@ public class AppCalc2 {
 
     public static void main(String[] args) {
 
+        //Operation mult = (n1, n2) -> n1 * n2;
+        
         //Simplificando uma expressão Lambda
         run((n1, n2) -> n1 + n2);
         run((n1, n2) -> n1 * n2);
