dbf2601ac9dd09dc246b17866f7f9bf669cd12a8
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-04-14 10:43:21 -0300

    Update Calculator.java

--- java-professional-1/java-lambda/Calculator.java
+++ java-professional-1/java-lambda/Calculator.java
@@ -3,9 +3,9 @@ public class Calculator {
     private int mult;
 
     int calcular(int x, int y) {
-        mult = 3;   //Está sendo usado como um atributo, por isso não é preciso ser final
         mult = 2;
-        Operation operation = (n1, n2) -> (n1 + n2) * mult;
-        return operation.calculate(x, y);
+        Operation operation = (n1, n2) -> (n1 + n2) * mult; //definindo a expressão
+        mult = 3;   //Está sendo usado como um atributo, por isso não é preciso ser final
+        return operation.calculate(x, y);   //executando a expressão - será utilizado o mult = 3
     }
 }
