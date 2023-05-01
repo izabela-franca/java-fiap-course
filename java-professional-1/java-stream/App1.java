4754f1173d46aabe50464c8444d14399b0b044d2
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-05-01 19:51:54 -0300

    Add files via upload

---
+++ java-professional-1/java-stream/App1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class App1 {
+
+    public static void main(String[] args) {
+
+        //Opção 1: criando uma Stream
+        Stream<String> s1 = Stream.of("A", "B", "C");
+
+        //Opção 2: criando uma Stream
+        String[] array = {"A", "B", "C"};
+        Stream<String> s2 = Arrays.stream(array);
+
+        //Opção 3: Criando uma Stream
+        List<String> list = List.of("A", "B", "C");
+        Stream<String> s3 = list.stream();
+
+        //Operação terminal
+        s1.forEach(System.out::println);
+    }
+}
